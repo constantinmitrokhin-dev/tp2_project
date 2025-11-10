b63367d531bdd793273ed485d2287ae1c9a93040
Guido Verde <guidoaverde@gmail.com>
2025-11-09 21:17:33 -0300

    loader: *Se creó la clase State *Se agregó el loader_export_states para exportar estados

---
+++ loader/src/classes/State.js
@@ -0,0 +1,13 @@
+
+class State{
+	constructor(name, iso3, iso2,country_code,country_name){
+		this.name = name;
+		this.iso3 = iso3;
+		this.iso2 = iso2;
+        this.country_code = country_code;
+        this.country_name = country_name;
+	}
+
+}
+
+module.exports = State;
