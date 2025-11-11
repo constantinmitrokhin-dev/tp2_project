902db5fead6a348351c82a02c130eed1759b3b71
Mitrokhin Constantin <constantin.mitrokhin@gmail.com>
2025-11-11 20:26:55 -0300

    Minor adjustemnts

--- bs/core/backend/seeds/CoreUser/CreateCoreUsers.js
+++ bs/core/backend/seeds/CoreUser/CreateCoreUsers.js
@@ -5,8 +5,8 @@ const CoreUser = require('../../models/core_user.js');
 module.exports = async function () {
 	const usersArray = [
 		{
-			name: 'José Ángel Carlos',
-			middle_name: "María's",
+			name: 'Carlos',
+			middle_name: 'María',
 			last_name: 'Perez',
 			user_name: 'c_p',
 			email: 'carlos.perez@mail.com',
