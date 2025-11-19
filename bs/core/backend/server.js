932a2f680acaeccacf4bc329e6d744bdcae385d5
Mitrokhin Constantin <constantin.mitrokhin@gmail.com>
2025-11-19 15:34:06 -0300

    Se eliminan dependencias obsoletas. Se ajusta server.js para reflejar el cambio.

--- bs/core/backend/server.js
+++ bs/core/backend/server.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/routes');
 const { PROJECT_NAME, ALLOWED_DOMAINS, ALLOWED_METHODS } = process.env;
@@ -11,23 +10,25 @@ const server = express();
 server.name = PROJECT_NAME;
 
 server.use(cors());
-server.use(express.json());
-server.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
-server.use(bodyParser.json({limit: '50mb'}));
+server.use(express.json({limit: '50mb'}));
+server.use(express.urlencoded({extended: true, limit: '50mb'}));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', ALLOWED_DOMAINS); // update to match the domain you will make the request from
 	res.header('Access-Control-Allow-Credentials', 'true');
 	// eslint-disable-next-line max-len
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, token, id, storename, user_email');
-	res.header('Access-Control-Allow-Methods', ALLOWED_METHODS);
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.header('Access-Control-Allow-Methods', ALLOWED_METHODS.split(','));
+	// res.cookie('token', jwt, {
+	// 	httpOnly: true,
+	// 	secure: true,       // obligatorio en HTTPS
+	// 	sameSite: 'None'    // necesario si el frontend está en otro dominio
+	// });
 	next();
 });
 
 server.use('/', routes);
-
-server.use(express.json());
 
 server.get('/', (req, res) => {
 	res.send('Core service running 🚀');
